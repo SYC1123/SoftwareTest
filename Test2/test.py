e0a31d2a018bafa0e75ddcc0e620c309b887a27f
syc1123 <874795069@qq.com>
2019-10-14 19:43:25 +0800

    软件测试

--- Test2/test.py
+++ Test2/test.py
@@ -6,15 +6,17 @@ def testPhoneNumber(number):
     '''
     number = str(number)
     if number.isdigit():
-        if len(number) < 7 or 7 < len(number) < 10:
-            print('请输入正确的电话号码位数')
+        if len(number) < 7 or 10 < len(number) or 7 < len(number) < 10:
+            print('错误！（请输入正确的电话号码位数）')
         else:
             after = number[-4:]
             before = number[-7:-4]
             if before[0] == '0' or before[0] == '1':
-                print('前缀码以"0"或”1”开头')
+                print('错误！（前缀码以"0"或”1”开头）')
+            else:
+                print('正确电话号码')
     else:
-        print('请输入数字号码')
+        print('错误！（请输入数字号码）')
 
 
 if __name__ == '__main__':
